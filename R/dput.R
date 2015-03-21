0efbe2a1bdf3e7bfe4b2ca971ea286fc054dfdc1
Roman Tsegelskyi <roman.tsegelskyi@gmail.com>
2015-03-21 13:40:03 -0400

    Improved dput

--- R/dput.R
+++ R/dput.R
@@ -1,38 +1,58 @@
 #' @export
-dputModified <- function (x, file = "", control = c("keepNA", "keepInteger", 
-                                                    "showAttributes")) 
-{
-  res <- lapply(x, function(l) capture.output(dputModifiedHelper(l, file, control)))
-  print(res)
+#' 
+dputModified <- function(x) {
+  res <- capture.output(dputModifiedHelper(x))
+  res
 }
 
-dputModifiedHelper <- function (x, file = "", control = c("keepNA", "keepInteger", 
-                                                          "showAttributes")) 
-{
-  if (is.character(file)) 
+dputModifiedHelper <- function (x,
+                   file = "",
+                   control = c("keepNA", "keepInteger", "showAttributes")){
+  if (is.character(file))
     if (nzchar(file)) {
       file <- file(file, "wt")
       on.exit(close(file))
-    }else file <- stdout()
+    }
+  else file <- stdout()
   opts <- .deparseOpts(control)
   if (isS4(x)) {
-    clx <- class(x)
-    cat("new(\"", clx, "\"\n", file = file, sep = "")
-    for (n in .slotNames(clx)) {
+    cat("new(\"", class(x), "\"\n", file = file, sep = "")
+    for (n in slotNames(x)) {
       cat("    ,", n, "= ", file = file)
-      slot.obj <- slot(x, n)
       if (is.language(slot.obj))
-        dput(enquote(slot(x, n)), file = file, control = control)
+        dputModified(enquote(slot(x, n)), file = file, control = control)
       else 
-        dput(slot(x, n), file = file, control = control)
+        dputModified(slot(x, n), file = file, control = control)
     }
     cat(")\n", file = file)
     invisible()
-  } 
+  } else if(length(grep('@',capture.output(str(x)))) > 0){
+    if(is.list(x) || is.vector(x)){
+      is.first <- TRUE
+      cat("list(\n", file = file, sep = "")
+      for (i in 1:length(x)) {
+        if(!is.null(names(x))){
+          n <- names(x)[i]
+          if(n != ''){
+            if (is.first) {
+              cat(n, "= ", file = file)
+              is.first <- FALSE
+            } else 
+              cat(" , ", n, "= ", file = file)
+          }
+        }
+        dputModified(x[[i]], file = file, control = control)
+      }
+      cat(")\n", file = file)
+      invisible()
+    } else {
+      stop('S4 objects are only handled if they are contained within an S4 object or a list/vector object')
+    }
+  }
   else {
     if (is.language(x))
       x <- enquote(x)
     if (is.list(x) || is.vector(x)) x <- lapply(x, function(x) if(is.language(x)) enquote(x) else x)
     .Internal(dput(x, file, opts))
-  } 
+  }
 }
